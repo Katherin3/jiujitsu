3dfd59ec80f1410bc78731b38410a6e4f218126c
eka <eka.gadaxabadze@gmail.com>
2026-07-16 00:07:33 +0400

    force logo cache refresh

--- components/MainHeader/MainHeader.tsx
+++ components/MainHeader/MainHeader.tsx
@@ -83,7 +83,7 @@ const MainHeader: React.FC<MainHeaderProps> = memo((props: MainHeaderProps) => {
 
     const logoCenterimage: JSX.Element = useMemo((): JSX.Element => {
         return (<Link href="/">
-            <Image src="https://res.cloudinary.com/dkd3pytpy/image/upload/c_pad,w_60,h_60,ar_1:1/logo192_fjgzrl_skgtuq" alt="Logo" width={60} height={60} />
+            <Image src="https://res.cloudinary.com/dkd3pytpy/image/upload/c_pad,w_60,h_60,ar_1:1/logo192_fjgzrl_skgtuq?v=2" alt="Logo" width={60} height={60} />
         </Link>);
     }, []);
 
